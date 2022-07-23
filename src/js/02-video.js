69ca58112a83e174d8b868b60e7a4cda89b2a550
oksyluka <oksana_l2@ukr.net>
2022-07-23 13:55:58 +0300

    Task 2 is done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const throttle = require('lodash.throttle');
+
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+};
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+
+
 
