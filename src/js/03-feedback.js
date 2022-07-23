cf42e8ba6a6e9bf3b66dfde0177866589f4ff959
oksyluka <oksana_l2@ukr.net>
2022-07-23 18:35:49 +0300

    Task 3 is done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+const form = document.querySelector('.feedback-form');
+const throttle = require('lodash.throttle');
 
+form.addEventListener('input', throttle(dataInput, 500));
+function dataInput(e) {
+  e.preventDefault();
+  let userData = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+}
+
+const savedData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+window.addEventListener('load', checkStorage);
+function checkStorage() {
+  if (savedData.email !== '' || savedData.message !== '') {
+    form.elements.email.value = savedData.email;
+    form.elements.message.value = savedData.message;
+  }
+}
+
+form.addEventListener('submit', handleSubmit);
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log(savedData);
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
+}
